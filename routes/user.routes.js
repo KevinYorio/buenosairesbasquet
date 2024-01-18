a5fc47fa87a195487ec654a9d76d1e46a0f165f5
Kevin Yorio <kevinyorio1310@gmail.com>
2024-01-17 22:37:34 -0300

    borrar

--- routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/user.routes.js
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user.controller');
-
-// Rutas
-router.post('/registro', userController.createUser);
-
-module.exports = { userRoutes: router };
